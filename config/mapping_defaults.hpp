3a5fd2dd4f6739b18e92e7c3f793324f05a9d914
Michele Tesser <michele.tesser@raceup.it>
2026-05-31 18:58:42 +0200

    remove mapping from this repo

---
+++ config/mapping_defaults.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "mapping_types.hpp"
+
+#ifndef RU_STM32H5XX_GPIO_MAP
+#define RU_STM32H5XX_GPIO_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_ADC_MAP
+#define RU_STM32H5XX_ADC_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_PWM_MAP
+#define RU_STM32H5XX_PWM_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_TIMER_MAP
+#define RU_STM32H5XX_TIMER_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_SERIAL_MAP
+#define RU_STM32H5XX_SERIAL_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_M_CAN_MAP
+#define RU_STM32H5XX_M_CAN_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_BX_CAN_MAP
+#define RU_STM32H5XX_BX_CAN_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_NV_MEMORY_MAP
+#define RU_STM32H5XX_NV_MEMORY_MAP(X)
+#endif
+
+#ifndef RU_STM32H5XX_WATCHDOG_MAP
+#define RU_STM32H5XX_WATCHDOG_MAP(X)
+#endif
